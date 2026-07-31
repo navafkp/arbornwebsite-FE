206e474109924e6b96f157c681724eb09fb62461
navafkp <255navaf@gmail.com>
2026-07-31 16:00:33 +0530

    chore: update production domain and temporarily disallow all web crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,13 +3,14 @@ import { SITE_URL } from "@/lib/site-config";
 
 export const dynamic = "force-static";
 
+// TEMPORARY: blocking all crawling until launch is ready.
+// To re-enable, revert to allow: "/" with the disallow list below.
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
-        disallow: ["/cart", "/checkout", "/profile", "/login", "/signup"],
+        disallow: "/",
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
